26c7ef9542f08109b99a6baec55915e46a95726d
胡子豪 <wuqieor@outlook.com>
2020-10-19 17:07:52 +0800

    解析XML

--- xml/src/Dom4jTest.java
+++ xml/src/Dom4jTest.java
@@ -1,5 +1,11 @@
+import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import pojo.Book;
 
 /**
  * @author huzihao
@@ -7,7 +13,35 @@ import org.junit.jupiter.api.Test;
  */
 public class Dom4jTest {
     @Test
-    public void test() {
-        var xmlRd = new SAXReader();
+    public void parsingXML() {
+        var reader = new SAXReader();
+        try {
+            var document = reader.read("src/books.xml");
+            System.out.println(document);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void parsingXMLToPojo() {
+        var reader = new SAXReader();
+        try {
+            var document = reader.read("src/books.xml");
+            var root = document.getRootElement();
+            var elementList = root.elements("book");
+            var bookList = new ArrayList<Book>();
+            elementList.forEach(element -> {
+                var id = element.attributeValue("id");
+                var name = element.elementText("name");
+                var author = element.elementText("author");
+                var price = BigDecimal.valueOf(Double.parseDouble(
+                        element.elementText("price")));
+                bookList.add(new Book(id, name, author, price));
+            });
+            System.out.println(bookList);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
     }
 }
