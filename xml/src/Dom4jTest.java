59781c060ee429d6b342302ca888a2315b1fa859
胡子豪 <wuqieor@outlook.com>
2020-10-19 16:39:31 +0800

    XML

---
+++ xml/src/Dom4jTest.java
@@ -0,0 +1,13 @@
+import org.dom4j.io.SAXReader;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author huzihao
+ * @since 2020/10/19 16:23
+ */
+public class Dom4jTest {
+    @Test
+    public void test() {
+        var xmlRd = new SAXReader();
+    }
+}
---
+++ xml/src/pojo/Book.java
@@ -0,0 +1,82 @@
+package pojo;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * @author huzihao
+ * @since 2020/10/19 16:20
+ */
+public class Book {
+    private String id;
+    private String name;
+    private String author;
+    private BigDecimal price;
+
+    public Book() {}
+
+    public Book(String id, String name, String author, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) &&
+                Objects.equals(name, book.name) &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(price, book.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, author, price);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
