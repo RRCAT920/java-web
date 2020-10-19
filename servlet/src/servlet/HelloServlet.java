c2c8fb2196a83d11767816cdfce06a9752870eeb
胡子豪 <wuqieor@outlook.com>
2020-10-20 00:28:31 +0800

    请求的分发处理以及最佳实践

--- servlet/src/servlet/HelloServlet.java
+++ servlet/src/servlet/HelloServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author huzihao
@@ -31,8 +32,12 @@ public class HelloServlet implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse)
             throws ServletException, IOException {
+        var request = (HttpServletRequest) servletRequest;
+        switch (request.getMethod()) {
+            case "GET" -> doGet();
+            case "POST" -> doPost();
+        }
         System.out.println("Life cycle 2: service");
-//        System.out.println("HelloServlet程序被访问");
     }
 
     @Override
@@ -43,5 +48,28 @@ public class HelloServlet implements Servlet {
     @Override
     public void destroy() {
         System.out.println("Life cycle 3: destroy");
+    }
+
+    /**
+     * 处理get请求
+     */
+    private void doGet() {
+        System.out.println("......get");
+        System.out.println("......get");
+        System.out.println("......get");
+        System.out.println("......get");
+        System.out.println("......get");
+    }
+
+    /**
+     * 处理post请求
+     */
+    private void doPost() {
+        System.out.println("...post...");
+        System.out.println("...post...");
+        System.out.println("...post...");
+        System.out.println("...post...");
+        System.out.println("...post...");
+        System.out.println("...post...");
     }
 }
