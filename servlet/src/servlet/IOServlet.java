538c055b7e44a27e5092372ed41b3cb4e702e54a
胡子豪 <wuqieor@outlook.com>
2020-10-20 20:43:25 +0800

    中文乱码问题：解决方案1（不推荐）

---
+++ servlet/src/servlet/IOServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author huzihao
+ * @since 2020/10/20 20:28
+ */
+public class IOServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Type", "text/html;charset=UTF-8");
+        var writer = response.getWriter();
+        writer.write("你好<br>");
+        writer.write("hello");
+//        response.getOutputStream();
+    }
+}
